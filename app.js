e6eb8e7aea05167380ea8714195605c294792523
Enzie Riddle <enzieriddle@gmail.com>
2019-10-10 20:29:18 -0500

    Attempt to apply animations to modal elements

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const apiURL = "https://randomuser.me/api/?results=12";
 const peopleList = document.getElementById("people");
-const modalPeopleList = document.getElementById("modal-people");
 const sectionClass = document.getElementsByClassName("section");
-const sectionButton = document.getElementsByTagName("BUTTON");
 const close = document.getElementsByClassName("close");
 
 /* Post profile information in index.html --------------------------*/
@@ -11,8 +9,7 @@ const close = document.getElementsByClassName("close");
 // Create the section variable to store each person in
 // Append each section to the peopleList div
 // Create HTML tags and store information within them
-
-async function generateHTML(data) {
+function generateHTML(data) {
   data.map(person => {
     const section = document.createElement("section");
     section.classList = "section";
@@ -26,21 +23,23 @@ async function generateHTML(data) {
       </button>
     `;
   });
-  for (i = 0; i <= sectionButton.length; i += 1) {
-    generateModal(sectionButton[i]);
-  }
+  const sectionButton = document.querySelectorAll(".section button");
+  sectionButton.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      generateModal(data[index]);
+    });
+  });
 }
-
 /* Generate modal information--------------------------*/
 // Take the section that was passed in and map each person
 // Create an element to hold the section in the modal
 // Append the section to the list of people in the modal
 // Set the inner HTML of the modal being viewed.
-async function generateModal(section, data) {
-  section.map(person => {
-    const modalSection = document.createElement("modalSection");
-    modalPeopleList.appendChild(modalSection);
-    modalSection.innerHTML = `
+async function generateModal(person) {
+  const modalPeopleList = document.getElementById("modal");
+  const modalSection = document.createElement("section");
+  modalPeopleList.appendChild(modalSection);
+  modalSection.innerHTML = `
       <img src=${person.picture.medium}>
       <h2>${person.name.first} ${person.name.last}</h2>
       <h3>${person.email}</h3>
@@ -48,9 +47,13 @@ async function generateModal(section, data) {
       <h3>${person.location.street} ${person.location.city} ${person.location.state} ${person.location.postcode}</h3>
       <h3>${person.dob.date}</h3>
     `;
-  });
+  // If user clicks the closing 'x', change display of modalPeopleList (id=modal-people) from "none" to "block"
+  window.onclick = function(event) {
+    if (event.target == modalPeopleList) {
+      modalPeopleList.style.display = "none";
+    }
+  };
 }
-
 /* Fetch the data--------------------------*/
 // Turn response into json
 // Send the data to the generateHTML function
@@ -58,12 +61,4 @@ async function generateModal(section, data) {
 fetch(apiURL)
   .then(response => response.json())
   .then(data => generateHTML(data.results))
-  .then(data => generateModal(sectionClass))
   .catch(error => console.log("Looks like there was a problem", error));
-
-// If user clicks the closing 'x', change display of modalPeopleList (id=modal-people) from "none" to "block"
-window.onclick = function(event) {
-  if (event.target == modalPeopleList) {
-    modalPeopleList.style.display = "none";
-  }
-};
